e4b4f89032498b8c65387a362bd2df872c69d1e5
Lev McKinney <levmckinney@gmail.com>
2019-07-15 01:23:42 -0400

    Added console warnings for yolol error

--- src/components/screens/Welcome.js
+++ src/components/screens/Welcome.js
@@ -7,14 +7,15 @@ export default function welcome() {
     <div className="bg-lite" style={{ height: "300vh" }}>
       <Card className="text-center text-white bg-dark mx-auto mt-5" style={{width:'20rem'}}>
       <Card.Title>Welcome to yoloxide web</Card.Title>
-      <Card.Subtitle>A cylon sponsored project.</Card.Subtitle>
+      <Card.Subtitle>A Cylon sponsored project.</Card.Subtitle>
       <Card.Text>To get started make your first  <Link to="/networks"><i>Device </i></Link>.
+      <p class="text-danger">Note that there is currently no backend so nothing will save when you reload the page!</p>
       </Card.Text>
       <Card.Footer>
         The react app is being maintained at <a href="https://github.com/levmckinney/yoloxide-web.git"> yoloxide-web </a>.
       </Card.Footer>
       <Card.Footer>
-        The yoloxide interrupter is at <a href="https://github.com/Jerald/yoloxide"> yoloxide </a>.
+        The yoloxide interpreter is at <a href="https://github.com/Jerald/yoloxide"> yoloxide </a>.
       </Card.Footer>
       </Card>
     </div>
--- src/yolol/executionEngine.js
+++ src/yolol/executionEngine.js
@@ -8,22 +8,10 @@ export default function stepDevice(device, wasmExecuteLine) {
     console.info("Passing into engin: ", {enginEnv, line})
     const newEnv = wasmExecuteLine(enginEnv, line);
     console.info("Got back out of engin: ", {newEnv})
+    if (newEnv.error !== "") {
+      console.warn(newEnv)
+    }
     return produce(device , (device) => {
-      // Object.entries(newEnv.global_context).forEach(([key, field]) => {
-      //   const fieldName = key.replace(/^:/g, '')
-      //   if(fieldName in device.dataFields) {
-      //     // need to get value inside of type wrapper
-      //     if(field.StringVal) {
-      //       device.dataFields[fieldName].value = field.StringVal
-      //     } else if(field.NumberVal){
-      //       device.dataFields[fieldName].value = (field.NumberVal/10000).toString()
-      //     } else {
-      //       throw Error("Invalid type in global env variable has no type")
-      //     }
-      //   } else {
-      //     console.error("trying to write to a field that dose not exist")
-      //   }
-      // })
       const variables = contextToVariables(newEnv.global_context, Object.keys(device.dataFields))
       variables.forEach(({name, value}) =>{
         device.dataFields[name].value = value
