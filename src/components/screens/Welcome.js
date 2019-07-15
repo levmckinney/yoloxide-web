0af41945dc9ccd2daa042d9012c79abc7ddbf5db
Lev McKinney <levmckinney@gmail.com>
2019-07-15 01:18:15 -0400

    fixed the welcome page

--- src/components/screens/Welcome.js
+++ src/components/screens/Welcome.js
@@ -4,11 +4,18 @@ import { Link } from 'react-router-dom'
 
 export default function welcome() {
   return (
-    <div className="bg-lite" style={{ height: "200vh" }}>
+    <div className="bg-lite" style={{ height: "300vh" }}>
       <Card className="text-center text-white bg-dark mx-auto mt-5" style={{width:'20rem'}}>
       <Card.Title>Welcome to yoloxide web</Card.Title>
-      <Card.Subtitle>Created by CYLON</Card.Subtitle>
-      <Card.Text>To get started make your first  <Link to="/networks"><i>Device </i></Link></Card.Text>
+      <Card.Subtitle>A cylon sponsored project.</Card.Subtitle>
+      <Card.Text>To get started make your first  <Link to="/networks"><i>Device </i></Link>.
+      </Card.Text>
+      <Card.Footer>
+        The react app is being maintained at <a href="https://github.com/levmckinney/yoloxide-web.git"> yoloxide-web </a>.
+      </Card.Footer>
+      <Card.Footer>
+        The yoloxide interrupter is at <a href="https://github.com/Jerald/yoloxide"> yoloxide </a>.
+      </Card.Footer>
       </Card>
     </div>
   );
