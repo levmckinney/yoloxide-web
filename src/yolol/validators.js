81fc9a891171c950708b001bf5d04fe4ce1f246b
Lev McKinney <levmckinney@gmail.com>
2019-07-18 01:43:28 -0400

    Fixed negative numbers

--- src/yolol/validators.js
+++ src/yolol/validators.js
@@ -1,6 +1,6 @@
-export const fieldNameRegex = /^[a-z_1-9]*$/
+export const fieldNameRegex = /^[a-z_0-9]*$/
 
-export const numberRegex=/(^([0-9]{1,14}(\.[0-9]{1,4})?)$)/
+export const numberRegex=/(^-?([0-9]{1,14}(\.[0-9]{1,4})?)$)/
 
 export const validNumber = (number) => {
   return typeof number === 'string' && numberRegex.test(number)
