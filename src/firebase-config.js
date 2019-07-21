1b5d9fdc3c9f1ab18e8880c5bc0637cb36ea23f1
Lev McKinney <levmckinney@gmail.com>
2019-07-20 21:41:56 -0400

    Set up firebase initialization and performance monitoring

---
+++ src/firebase-config.js
@@ -0,0 +1,3 @@
+export default {
+  // Your config here
+}
--- src/index.js
+++ src/index.js
@@ -6,16 +6,30 @@ import {createStore, applyMiddleware} from 'redux'
 import rootReducer from './reducers'
 import {initialNetworks} from './reducers/networks';
 import {createLogger} from 'redux-logger'
+import firebaseConfig from './firebase-config';
 
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+
+// Add the Performance Monitoring library
+import "firebase/performance";
+
+
+firebase.initializeApp(firebaseConfig);
+firebase.performance();
+
+// Setting up redux action logging
 const logger = createLogger({
   level:'info'
 });
 
+//redux
 export const initialStore = {
   networks: initialNetworks
 }
 
 const store = createStore(rootReducer, initialStore ,applyMiddleware(logger));
 
+//react
 ReactDOM.render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
 
