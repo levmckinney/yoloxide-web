6407e9c09a5b44573140730904955faf5b9cc326
Lev McKinney <levmckinney@gmail.com>
2019-08-23 04:40:52 -0400

    fixed data fields removing themselfs

--- src/components/util/RemoveButton.js
+++ src/components/util/RemoveButton.js
@@ -3,6 +3,6 @@ import Button from 'react-bootstrap/Button'
 
 export default function RemoveButton({remove, size}={size:'sm'}) {
   return (
-    <Button variant="outline-danger" size={size} onClick={()=>{remove()}}>Remove</Button>
+    <Button variant="outline-danger" size={size} onClick={()=>remove()}>Remove</Button>
   )
 }
